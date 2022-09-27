611208d3b1d03734df5467e5a209fa26e835077d
drbobby27 <envioshseq@gmail.com>
2022-09-26 23:42:47 -0500

    [Delete] Figure out assembler liquidation

--- js/assembler.js
+++ js/assembler.js
@@ -34,6 +34,49 @@ new Vue({
               timer
             })
     },
+    validateInputs() {
+        
+        error = false;
+      if (this.fullName === "") {
+        this.errors.name = true;
+        error = true;
+      } else {
+        this.errors.name = false;
+      }
+      if (this.identityNumber === "") {
+        this.errors.lastName = true;
+        error = true;
+      } else {
+        this.errors.lastName = false;
+      }
+      if (this.baseSalary === "") {
+        this.errors.identity = true;
+        error = true;
+      } else {
+        this.errors.identity = false;
+      }
+      if (this.extraHoursQuantity === "") {
+        this.errors.position = true;
+        error = true;
+      } else {
+        this.errors.position = false;
+      }
+      if (this.subsidy === "") {
+        this.errors.age = true;
+        error = true;
+      } else {
+        this.errors.age = false;
+      }
+      if (this.bonus == "") {
+        this.errors.sex = true;
+        error = true;
+      } else {
+        this.errors.sex = false;
+      }if(this.comission === ""){
+        return  this.errors.age = false;
+      }
+      return error;
+    },
     cleanInputs(){
         this.fullName ="",
         this.identityNumber= "",
@@ -87,6 +130,38 @@ new Vue({
         })
         this.updateLocalStorage()
         this.cleanInputs()
+    },
+    message(title,timer,position,text){
+        Swal.fire({
+          position,
+          text,
+          icon: "success",
+          title,
+          showConfirmButton: false,
+          timer
+        })},
+    deleteAlert(item) {
+        Swal.fire({
+            title: "¿Está seguro de eliminar?",
+            text: "¡Este proceso es irreversible!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "SI",
+            cancelButtonText: "NO",
+          }).then((result) => {
+            if (result.isConfirmed) {
+              this.consolidationLiquidations.splice(this.consolidationLiquidations.indexOf(item), 1)
+              this.message(
+                "Se eliminó correctamente",
+                1600,
+                "center",
+                "¡Este proceso es irreversible!"
+              )
+              this.updateLocalStorage()
+            }
+          })
     }
     }
 })
