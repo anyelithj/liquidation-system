9bbaf106e68e88c48a7df2faef624e860561f29d
Anyelith Jacobo <anyelithj@misena.edu.co>
2022-09-27 12:15:48 -0500

    developer of logic the login commit

--- js/login.js
+++ js/login.js
@@ -1,6 +1,61 @@
 var  app = new Vue( {
     el: '#app',
     data: {
+        position:["administrador", "secretario","vendedor","ensamblador"],
+        optionPosition:"",
+        pin:"",
+        userCredentials: [
+            { position: "administrador", pin: "43891v569" },
+            { position: "secretario", pin: "23a138" },
+            { position: "vendedor", pin: "328y46910" },
+            { position: "ensamblador", pin: "522376t" },
+            { position: "ensamblador", pin:"56789q" },
+            { position: "secretario", pin: "7865677a" },
+          ],
+        errorPosition:false,
+        errorPin: false,
         
+    },
+    methods: {
+        login() {
+          
+        },
+        clearForm(){
+            this.optionPosition = "",
+            this.pin = ""
+        },
+        validateCredentials(user, key) {
+            let loguedUser = [];
+            let session = this.userCredentials.filter((user)=> user.position === user && user.pin === key)
+            console.log(session)
+            loguedUser = [...session];
+            return loguedUser.length === 0? this.message("Oops",2200,"center","Verifique que los datos sean correctos","error"):
+            this.message("¡Datos correctos!", 2200,"center","Ingreso exito", "sucess");
+
+        },
+        message(title, timer, position, text, icon){
+            Swal.fire({
+                position,
+                text,
+                icon,
+                title,
+                showConfirmButton: false,
+                timer,
+              });
+        },
+        getErrorPosition(){
+            if(this.position === "") {
+                this.errorPosition = true;
+            } else {
+                this.errorPosition = false;
+            }
+        },
+        getErrorPin(){
+            if(this.pin === "") {
+                this.errorPin = true;
+            } else {
+                this.errorPin = false;
+            }
+        }
     }
 })
