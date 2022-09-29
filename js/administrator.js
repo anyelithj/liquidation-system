e42ceeb30be7ce423ebd49a95a990795a5a245c3
drbobby27 <envioshseq@gmail.com>
2022-09-28 22:30:01 -0500

    Storage

--- js/administrator.js
+++ js/administrator.js
@@ -3,27 +3,35 @@ new Vue({
     data: {
        defaultRol: null,
        wholeRoles: ['Secretario','Vendedor','Ensamblador'],
-       errors:{
-        selectOptions: false,
-        assembler: {
+       errorOptions: false, 
+
+       errorsAssembler:{
           baseSalary: false,
-          shoesMaxQuantity: false,
-          tennisMaxQuantity: false
+          minPercentShoes:false,
+          maxPercentShoes:false,
+          minPercentTennis:false,
+          maxPercentTennis:false,
+          maxShoesQuantity:false,
+          maxTennisQuantity:false
         },
-        secretary: {
+       errorsSeller: {
+          baseSalary: false,
+          minComission: false,
+          maxComission: false
+       },
+       errorsSecretary: {
           baseSalary: false,
           extraHoursPercent: false
-        },
-        seller: {
-          baseSalary: false,
-          comission: false
-        }
        },
        inputRoles: {
         assembler: {
           baseSalary: "",
-          shoesMaxQuantity: "",
-          tennisMaxQuantity: ""
+          minPercentShoes: "",
+          maxPercentShoes: "",
+          minPercentTennis: "",
+          maxPercentTennis: "",
+          maxShoesQuantity: "",
+          maxTennisQuantity: ""
         },
         secretary: {
           baseSalary: "",
@@ -31,62 +39,15 @@ new Vue({
         },
         seller: {
           baseSalary: "",
-          comission: ""
+          minComission: "",
+          maxComission: ""
         }
        },
-       identityNumber: "",
-       baseSalary: "",
-       extraHoursQuantity:"",
-       subsidy: "",
-       bonus: "",
-       comission:"",
-       resultLiquidation: 0, 
-       adminData : [],
-       adminPrivileges: [
-        {
-          rol: 'Secretario',
-          values: {
-            baseSalary: ( salary = 2500000 ) => salary,
-            extraHours: ( percent = 180 ) =>  percent/100
-          }
-         },
-       {
-        rol: 'Vendedor',
-        values: {
-          baseSalary: ( inputValue = 3000000 ) => inputValue,
-          comission: (baseSalary= 3000000, inputValue, comission1= .10, comission2= .20) => {
-            if(inputValue > 5000000 && inputValue <= 10000000) {
-              return baseSalary * comission1
-            }else if(inputValue < 10000000) {
-              return baseSalary *comission2
-            }return 0
-          } 
-        }
-       },
-       {
-        rol: 'Ensamblador',
-        values: {
-          baseSalary: ( inputValue = 2800000 ) => inputValue,
-          hourPrice: (baseSalary = 2800000) => baseSalary/160,
-          extraHours: (hours) => hours * (hourPrice * 2.2),
-          subsidy: (inputValue) => inputValue,
-          comissionShoes: (quantity, salary, min=1000, max=2000) => {
-            if(quantity > min && quantity <= max){
-                return  salary * .10
-            }else  if(quantity > max ){
-                return salary * .20
-            }else  return 0
-          },
-          comissionTennis: (quantity,salary) => {
-            if(quantity > 1700 && quantity < 2000){
-              return  salary * .15
-          }else  if(quantity > 3000){
-                return  salary * .30
-            }else  return 0
-          }
-        }
-       }
-       ],
+       Secretary: [],
+       Assembler: [],
+       Seller: [],
+       adminDefaultData : [],
+       adminGetterData: [],
        ADMIN_KEY: "setAdminDataStorage",
        SECRETARY_STORAGE_KEY: "setSecretaryDataStorage",
        SELLER_STORAGE_KEY: "setSellerDataStorage",
@@ -94,15 +55,73 @@ new Vue({
     },
     created(){
         this.updateLocalStorage()
-        this.adminData = JSON.parse(localStorage.getItem(this.ADMIN_KEY) || '[]')
+        this.adminGetterData = JSON.parse(localStorage.getItem(this.ADMIN_KEY) || '[]')
       },
     methods: {
       showData (){
-        console.log(this.adminData)
+        // this.adminDefaultData[0].values.baseSalary.replace(/['"]+/g, '')
+         console.log(this.adminGetterData)
       },
-        updateLocalStorage(){
-            return localStorage.setItem(this.ADMIN_KEY, JSON.stringify(this.adminPrivileges))
-          },
+      updateLocalStorage(){
+        this.updateAdminData()
+        return localStorage.setItem(this.ADMIN_KEY, JSON.stringify(this.adminDefaultData))
+      },
+      updateAdminData(){
+        return this.adminDefaultData = [...this.Secretary, ...this.Assembler, ...this.Seller]
+      },
+      defineComissionShoes (quantity, salary, min=1000, max=2000) {
+        if(quantity > min && quantity <= max){
+            return  salary * .10
+        }else  if(quantity > max ){
+            return salary * .20
+        }else  return 0
+      },
+      defineComissionTennis (max=3000, min=1700 ,quantity,salary )  {
+        if(quantity > min && quantity < 2000){
+          return  salary * .15
+      }else  if(quantity > 3000){
+            return  salary * .30
+        }else  return 0
+      },
+      setDataSecretary(){
+        this.Secretary = [{
+          baseSalary: this.inputRoles.secretary.baseSalary,
+          extraHours:  this.inputRoles.secretary.extraHoursPercent/100
+        }]
+        console.log(this.Secretary)
+        this.inputRoles.secretary.baseSalary = ""
+        this.inputRoles.secretary.extraHoursPercent = ""
+      },
+      setDataAssembler(){
+        this.Assembler = [{
+          baseSalary: this.inputRoles.assembler.baseSalary,
+          minPercentShoes: "",
+          maxPercentShoes: "",
+          minPercentTennis: "",
+          maxPercentTennis: "",
+          maxShoesQuantity: "",
+          maxTennisQuantity: ""
+        }]
+        console.log(this.Assembler)
+        this.inputRoles.assembler.baseSalary = ""
+        this.inputRoles.assembler.minPercentShoes = ""
+        this.inputRoles.assembler.maxPercentShoes = ""
+        this.inputRoles.assembler.minPercentTennis = ""
+        this.inputRoles.assembler.maxPercentTennis = ""
+        this.inputRoles.assembler.maxShoesQuantity = ""
+        this.inputRoles.assembler.maxTennisQuantity = ""
+      },
+      setDataSeller(){
+        this.Seller = [{
+          baseSalary: this.inputRoles.seller.baseSalary,
+          minComission: this.inputRoles.seller.minComission,
+          maxComission: this.inputRoles.seller.maxComission
+        }]
+        console.log(this.Seller)
+        this.inputRoles.seller.baseSalary = ""
+        this.inputRoles.seller.minComission = ""
+        this.inputRoles.seller.maxComission = ""
+      },
         message(title,timer,position,text){
             Swal.fire({
               position,
@@ -157,16 +176,6 @@ new Vue({
       }
       return error;
     },
-    cleanInputs(){
-        this.fullName ="",
-        this.identityNumber= "",
-        this.baseSalary= "",
-        this.extraHoursQuantity= "",
-        this.subsidy= "",
-        this.bonus= "",
-        this.comission=""
-    },
-    
     showFormatedNumber(value){
         function thousandSeparator(number = 0, decimalsQuantity = 2) {
             return Number(number).toFixed(decimalsQuantity).replace(/\B(?=(\d{3})+(?!\d))/g, ",");
