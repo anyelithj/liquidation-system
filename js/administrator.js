3bf9891ccb1a9a320edc30e43d3785c63725025d
drbobby27 <envioshseq@gmail.com>
2022-09-29 10:37:44 -0500

    Update Administrator Data and save method

--- js/administrator.js
+++ js/administrator.js
@@ -1,195 +1,214 @@
 new Vue({
-    el: '#app',
-    data: {
-       defaultRol: null,
-       wholeRoles: ['Secretario','Vendedor','Ensamblador'],
-       errorOptions: false, 
-
-       errorsAssembler:{
-          baseSalary: false,
-          minPercentShoes:false,
-          maxPercentShoes:false,
-          minPercentTennis:false,
-          maxPercentTennis:false,
-          maxShoesQuantity:false,
-          maxTennisQuantity:false
-        },
-       errorsSeller: {
-          baseSalary: false,
-          minComission: false,
-          maxComission: false
-       },
-       errorsSecretary: {
-          baseSalary: false,
-          extraHoursPercent: false
-       },
-       inputRoles: {
-        assembler: {
-          baseSalary: "",
-          minPercentShoes: "",
-          maxPercentShoes: "",
-          minPercentTennis: "",
-          maxPercentTennis: "",
-          maxShoesQuantity: "",
-          maxTennisQuantity: ""
-        },
-        secretary: {
-          baseSalary: "",
-          extraHoursPercent: ""
-        },
-        seller: {
-          baseSalary: "",
-          minComission: "",
-          maxComission: ""
-        }
-       },
-       Secretary: [],
-       Assembler: [],
-       Seller: [],
-       adminDefaultData : [],
-       adminGetterData: [],
-       ADMIN_KEY: "setAdminDataStorage",
-       SECRETARY_STORAGE_KEY: "setSecretaryDataStorage",
-       SELLER_STORAGE_KEY: "setSellerDataStorage",
-       ASSEMBLER_STORAGE_KEY: "setAssemblerDataStorage",
+  el: '#app',
+  data: {
+     defaultRol: null,
+     wholeRoles: ['Secretario','Vendedor','Ensamblador'],
+     errorOptions: false, 
+     errorsAssembler:{
+        baseSalary: false,
+        minPercentShoes:false,
+        maxPercentShoes:false,
+        minPercentTennis:false,
+        maxPercentTennis:false,
+        maxShoesQuantity:false,
+        maxTennisQuantity:false
+      },
+     errorsSeller: {
+        baseSalary: false,
+        minComission: false,
+        maxComission: false
+     },
+     errorsSecretary: {
+        baseSalary: false,
+        extraHoursPercent: false
+     },
+     inputRoles: {
+      assembler: {
+        baseSalary: "",
+        minPercentShoes: "",
+        maxPercentShoes: "",
+        minPercentTennis: "",
+        maxPercentTennis: "",
+        maxShoesQuantity: "",
+        maxTennisQuantity: ""
+      },
+      secretary: {
+        baseSalary: "",
+        extraHoursPercent: ""
+      },
+      seller: {
+        baseSalary: "",
+        minComission: "",
+        maxComission: ""
+      }
+     },
+     Secretary: [],
+     Assembler: [],
+     Seller: [],
+     adminDefaultData : [],
+     adminGetterData: [],
+     ADMIN_KEY: "setAdminDataStorage",
+     SECRETARY_STORAGE_KEY: "setSecretaryDataStorage",
+     SELLER_STORAGE_KEY: "setSellerDataStorage",
+     ASSEMBLER_STORAGE_KEY: "setAssemblerDataStorage",
+  },
+  created(){
+      this.updateLocalStorage()
+      this.adminGetterData = JSON.parse(localStorage.getItem(this.ADMIN_KEY) || '[]')
     },
-    created(){
-        this.updateLocalStorage()
-        this.adminGetterData = JSON.parse(localStorage.getItem(this.ADMIN_KEY) || '[]')
-      },
-    methods: {
-      showData (){
-        // this.adminDefaultData[0].values.baseSalary.replace(/['"]+/g, '')
-         console.log(this.adminGetterData)
-      },
-      updateLocalStorage(){
-        this.updateAdminData()
-        return localStorage.setItem(this.ADMIN_KEY, JSON.stringify(this.adminDefaultData))
-      },
-      updateAdminData(){
-        return this.adminDefaultData = [...this.Secretary, ...this.Assembler, ...this.Seller]
-      },
-      defineComissionShoes (quantity, salary, min=1000, max=2000) {
-        if(quantity > min && quantity <= max){
-            return  salary * .10
-        }else  if(quantity > max ){
-            return salary * .20
-        }else  return 0
-      },
-      defineComissionTennis (max=3000, min=1700 ,quantity,salary )  {
-        if(quantity > min && quantity < 2000){
-          return  salary * .15
-      }else  if(quantity > 3000){
-            return  salary * .30
-        }else  return 0
-      },
-      setDataSecretary(){
-        this.Secretary = [{
-          baseSalary: this.inputRoles.secretary.baseSalary,
-          extraHours:  this.inputRoles.secretary.extraHoursPercent/100
-        }]
-        console.log(this.Secretary)
-        this.inputRoles.secretary.baseSalary = ""
-        this.inputRoles.secretary.extraHoursPercent = ""
-      },
-      setDataAssembler(){
-        this.Assembler = [{
-          baseSalary: this.inputRoles.assembler.baseSalary,
-          minPercentShoes: "",
-          maxPercentShoes: "",
-          minPercentTennis: "",
-          maxPercentTennis: "",
-          maxShoesQuantity: "",
-          maxTennisQuantity: ""
-        }]
-        console.log(this.Assembler)
-        this.inputRoles.assembler.baseSalary = ""
-        this.inputRoles.assembler.minPercentShoes = ""
-        this.inputRoles.assembler.maxPercentShoes = ""
-        this.inputRoles.assembler.minPercentTennis = ""
-        this.inputRoles.assembler.maxPercentTennis = ""
-        this.inputRoles.assembler.maxShoesQuantity = ""
-        this.inputRoles.assembler.maxTennisQuantity = ""
-      },
-      setDataSeller(){
-        this.Seller = [{
-          baseSalary: this.inputRoles.seller.baseSalary,
-          minComission: this.inputRoles.seller.minComission,
-          maxComission: this.inputRoles.seller.maxComission
-        }]
-        console.log(this.Seller)
-        this.inputRoles.seller.baseSalary = ""
-        this.inputRoles.seller.minComission = ""
-        this.inputRoles.seller.maxComission = ""
-      },
-        message(title,timer,position,text){
-            Swal.fire({
-              position,
-              text,
-              icon: "success",
-              title,
-              showConfirmButton: false,
-              timer
-            })
+  methods: {
+    showData (){
+      // this.adminDefaultData[0].values.baseSalary.replace(/['"]+/g, '')
+       console.log(this.adminGetterData)
     },
-    validateInputs() {
-        error = false;
-      if (this.fullName === "" ) {
-        this.errors.fullName = true;
-        error = true;
-      } else {
-        this.errors.fullName = false;
-      }
-      if (this.identityNumber === "" || typeof this.fullName !== 'number') {
-        this.errors.identityNumber = true;
-        error = true;
-      } else {
-        this.errors.identityNumber = false;
-      }
-      if (this.baseSalary === "" || typeof this.fullName !== 'number') {
-        this.errors.baseSalary = true;
-        error = true;
-      } else {
-        this.errors.baseSalary = false;
-      }
-      if (this.extraHoursQuantity === "" || typeof this.fullName !== 'number') {
-        this.errors.extraHoursQuantity = true;
-        error = true;
-      } else {
-        this.errors.extraHoursQuantity = false;
-      }
-      if (this.subsidy === "" || typeof this.fullName !== 'number') {
-        this.errors.subsidy = true;
-        error = true;
-      } else {
-        this.errors.subsidy = false;
-      }
-      if (this.bonus == "" || typeof this.fullName !== 'number') {
-        this.errors.bonus = true;
-        error = true;
-      } else {
-        this.errors.bonus = false;
-      }if(this.comission === "" || typeof this.fullName !== 'number'){
-         this.errors.comission = true;
-      }else{
-        this.errors.comission = false
-      }
-      return error;
+    updateLocalStorage(){
+      this.updateAdminData()
+      return localStorage.setItem(this.ADMIN_KEY, JSON.stringify(this.adminDefaultData))
     },
-    showFormatedNumber(value){
-        function thousandSeparator(number = 0, decimalsQuantity = 2) {
-            return Number(number).toFixed(decimalsQuantity).replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-          }
-          return thousandSeparator(value)
+    updateAdminData(){
+      return this.adminDefaultData = [...this.Secretary, ...this.Assembler, ...this.Seller]
     },
-    message(title,timer,position,text){
-        Swal.fire({
-          position,
-          text,
-          icon: "success",
-          title,
-          showConfirmButton: false,
-          timer
-        })}
+    defineComissionShoes (quantity, salary, min=1000, max=2000) {
+      if(quantity > min && quantity <= max){
+          return  salary * .10
+      }else  if(quantity > max ){
+          return salary * .20
+      }else  return 0
+    },
+    defineComissionTennis (max=3000, min=1700 ,quantity,salary )  {
+      if(quantity > min && quantity < 2000){
+        return  salary * .15
+    }else  if(quantity > 3000){
+          return  salary * .30
+      }else  return 0
+    },
+    setLocalStorageData(key,data){
+      localStorage.setItem(key, JSON.stringify(data))
+    },
+    setDataSecretary(){        
+      this.Secretary = {
+        baseSalary: this.inputRoles.secretary.baseSalary,
+        extraHours:  this.inputRoles.secretary.extraHoursPercent/100
+      }        
+      this.setLocalStorageData(this.SECRETARY_STORAGE_KEY, this.Secretary)
+      console.log(this.Secretary)
+      this.message(
+        "¡Enhorabuena!",
+        2000,
+        "center",
+        "¡Los cambios fueron guardados correctamente!",
+        false
+      )
+      this.inputRoles.secretary.baseSalary = ""
+      this.inputRoles.secretary.extraHoursPercent = ""
+    },
+   
+    setDataAssembler(){      
+      this.Assembler = {
+        baseSalary: this.inputRoles.assembler.baseSalary,
+        minPercentShoes: this.inputRoles.assembler.minPercentShoes,
+        maxPercentShoes: this.inputRoles.assembler.maxPercentShoes,
+        minPercentTennis: this.inputRoles.assembler.minPercentTennis,
+        maxPercentTennis: this.inputRoles.assembler.maxPercentTennis,
+        maxShoesQuantity: this.inputRoles.assembler.maxShoesQuantity,
+        maxTennisQuantity: this.inputRoles.assembler.maxTennisQuantity
+      }
+      this.setLocalStorageData(this.ASSEMBLER_STORAGE_KEY,this.Assembler)
+      console.log(this.Assembler)
+      this.message(
+        "¡Enhorabuena!",
+        2000,
+        "center",
+        "¡Los cambios fueron guardados correctamente!",
+        false
+      )
+      this.inputRoles.assembler.baseSalary = ""
+      this.inputRoles.assembler.minPercentShoes = ""
+      this.inputRoles.assembler.maxPercentShoes = ""
+      this.inputRoles.assembler.minPercentTennis = ""
+      this.inputRoles.assembler.maxPercentTennis = ""
+      this.inputRoles.assembler.maxShoesQuantity = ""
+      this.inputRoles.assembler.maxTennisQuantity = ""
+    },
+    setDataSeller(){     
+      this.Seller = {
+        baseSalary: this.inputRoles.seller.baseSalary,
+        minComission: this.inputRoles.seller.minComission,
+        maxComission: this.inputRoles.seller.maxComission
+      }
+      this.setLocalStorageData(this.SELLER_STORAGE_KEY,this.Seller)
+      console.log(this.Seller)
+      this.message(
+        "¡Enhorabuena!",
+        2000,
+        "center",
+        "¡Los cambios fueron guardados correctamente!",
+        false
+      )
+      this.inputRoles.seller.baseSalary = ""
+      this.inputRoles.seller.minComission = ""
+      this.inputRoles.seller.maxComission = ""
+    },
+      message(title,timer,position,text, button){
+          swal({
+            position,
+            text,
+            icon: "success",
+            title,
+            dangerMode: false,
+            timer,
+            button
+          })
+  },
+  validateInputs() {
+      error = false;
+    if (this.fullName === "" ) {
+      this.errors.fullName = true;
+      error = true;
+    } else {
+      this.errors.fullName = false;
     }
+    if (this.identityNumber === "" || typeof this.fullName !== 'number') {
+      this.errors.identityNumber = true;
+      error = true;
+    } else {
+      this.errors.identityNumber = false;
+    }
+    if (this.baseSalary === "" || typeof this.fullName !== 'number') {
+      this.errors.baseSalary = true;
+      error = true;
+    } else {
+      this.errors.baseSalary = false;
+    }
+    if (this.extraHoursQuantity === "" || typeof this.fullName !== 'number') {
+      this.errors.extraHoursQuantity = true;
+      error = true;
+    } else {
+      this.errors.extraHoursQuantity = false;
+    }
+    if (this.subsidy === "" || typeof this.fullName !== 'number') {
+      this.errors.subsidy = true;
+      error = true;
+    } else {
+      this.errors.subsidy = false;
+    }
+    if (this.bonus == "" || typeof this.fullName !== 'number') {
+      this.errors.bonus = true;
+      error = true;
+    } else {
+      this.errors.bonus = false;
+    }if(this.comission === "" || typeof this.fullName !== 'number'){
+       this.errors.comission = true;
+    }else{
+      this.errors.comission = false
+    }
+    return error
+  },
+  showFormatedNumber(value){
+      function thousandSeparator(number = 0, decimalsQuantity = 2) {
+          return Number(number).toFixed(decimalsQuantity).replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+        }
+        return thousandSeparator(value)
+  }
+  }
 })
--- js/login.js
+++ js/login.js
@@ -1,4 +1,4 @@
-var app = new Vue({
+new Vue({
   el: "#app",
   data: {
     position: ["administrador", "secretario", "vendedor", "ensamblador"],
@@ -10,7 +10,7 @@ var app = new Vue({
       { position: "vendedor", pin: "32846910" },
       { position: "ensamblador", pin: "522376" },
       { position: "ensamblador", pin: "56789" },
-      { position: "secretario", pin: "7865677" },
+      { position: "secretario", pin: "123" },
     ],
     errorPosition: false,
     errorPin: false,
@@ -22,29 +22,59 @@ var app = new Vue({
     },
     validateCredentials(user, key) {
       this.fieldValidations(user, key)
-      console.log(user)
-      console.log(typeof key)
       let loguedUser = [];
       let session = this.userCredentials.filter(
         (({position, pin})  => position === user && pin === key)
       );
-      console.log(session);
       loguedUser = [...session];
-      return loguedUser.length === 0
-        ? this.message(
-            "Oops",
-            2200,
-            "center",
-            "Verifique que los datos sean correctos",
-            "error"
-          )
-        : this.message(
+      loguedUser.length === 0 ? this.message( "Oops", 3200,"center","Verifique que los datos sean correctos","error"): this.Session(loguedUser);
+      return this.clearForm();
+    },
+    Session(data){
+      console.log(data)
+      let info = data[0].position;
+      if(info === "vendedor") {
+        this.message(
+          "¡Datos correctos!",
+          3200,
+          "center",
+          "Ingreso exito",
+          "success"
+          );
+          console.log(info)
+          window.location.href = '../view/seller.html'
+      } else if(info === "ensamblador") {
+        this.message(
+          "¡Datos correctos!",
+          3200,
+          "center",
+          "Ingreso exito",
+          "success"
+          );
+          console.log(info)
+          window.location.href = '../view/assembler.html'
+      } else if(info === "secretario") {
+        console.log(info)
+          this.message(
             "¡Datos correctos!",
-            2200,
+            3200,
             "center",
             "Ingreso exito",
             "success"
           );
+          window.location.href = '../view/secretary.html'
+      } else if(info === "administrador") {
+        console.log(info)
+          this.message(
+            "¡Datos correctos!",
+            3200,
+            "center",
+            "Ingreso exito",
+            "success"
+          );
+          window.location.href = '../view/administrator.html'
+      }
+          console.log(info)
     },
     fieldValidations() {
       error = false;
@@ -63,7 +93,7 @@ var app = new Vue({
       return error;
     },
     message(title, timer, position, text, icon) {
-      Swal.fire({
+      console.log({
         position,
         text,
         icon,
